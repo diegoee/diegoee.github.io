ab3f05c91c8a9bb014882a9260ba8da739fa6044
Diego E <despinolaespigares@gmail.com>
2019-11-30 12:32:19 +0100

    .

--- apps/scriptTest/test01.js
+++ apps/scriptTest/test01.js
@@ -8,12 +8,20 @@ var app = {
   },  
   exe: async function(){
     var moment = require('moment'); 
+    var api = require('termux');
+
     var time = moment();
     console.log(moment.utc(moment().diff(time)).format("HH:mm:ss.SSS")+' -> Start Exe Test');   
     console.log('start sleep');
-    await this.sleep(1000).then(function(){       
+    await this.sleep(10).then(function(){       
       console.log('exe fn then');  
     });  
+ 
+    if (api.hasTermux){  
+      console.log('Termux API'); 
+      api.toast('Hello word!').run();
+    }
+
     console.log('start sleep'); 
     console.log(moment.utc(moment().diff(time)).format("HH:mm:ss.SSS")+' -> End Exe Test');  
   }, 
