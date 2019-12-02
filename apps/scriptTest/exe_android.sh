64f167f75d67a752176369ca4a3eccaee6ae150d
Diego E <despinolaespigares@gmail.com>
2019-12-02 20:26:51 +0100

    .

--- apps/scriptTest/exe_android.sh
+++ apps/scriptTest/exe_android.sh
@@ -1,1 +1,7 @@
-#test
+#!/usr/bin/env bash
+clear
+echo "exe test script"
+rm -r storage/shared/temp
+svn export https://github.com/diegoee/diegoee.github.io/trunk/apps/scriptTest storage/shared/temp/
+npm install --prefix storage/shared/temp/
+node storage/shared/temp/test01.js
