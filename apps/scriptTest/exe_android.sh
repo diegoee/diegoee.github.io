62bc1633c8266e71f5c4a629811f931ce4f4b9c7
Diego E <despinolaespigares@gmail.com>
2019-12-03 22:14:14 +0100

    .

--- apps/scriptTest/exe_android.sh
+++ apps/scriptTest/exe_android.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-echo "************************** EXE scrip test **************************"
+echo "******************* EXE scrip test "
 rm -r storage/shared/temp
-echo "************************** SVN exporting ..."
+echo "******************* SVN exporting ..."
 svn export https://github.com/diegoee/diegoee.github.io/trunk/apps/scriptTest storage/shared/temp/
-echo "************************** installing node modules ..."
+echo "******************* installing node modules ..."
 npm install --prefix storage/shared/temp/
-echo "************************** Exe node ..."
+echo "******************* Exe node ..."
 node storage/shared/temp/test01.js
