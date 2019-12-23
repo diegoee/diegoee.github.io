9b90d9fe2c00ca2f40e0dc44bc760ca68e866fed
Diego E <despinolaespigares@gmail.com>
2019-12-23 14:38:56 +0100

    .

--- apps/myPortfolio/android_termux_exe.sh
+++ apps/myPortfolio/android_termux_exe.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "******************* EXE scrip test "
-rm -r storage/shared/Downloads/temp
+rm -r storage/shared/temp
 echo "******************* SVN exporting ..."
-svn export https://github.com/diegoee/diegoee.github.io/trunk/apps/myPortfolio storage/shared/Downloads/temp/
+svn export https://github.com/diegoee/diegoee.github.io/trunk/apps/myPortfolio storage/shared/temp/
 echo "******************* installing node modules ..."
-npm install --prefix storage/Downloads/temp/
+npm install --prefix storage/temp/
 echo "******************* Exe node ..."
-node node storage/shared/Downloads/appPortfolio.js exe storage/shared/Downloads/Movements.xls 
+node storage/shared/temp/appPortfolio.js exe storage/shared/Download/Movements.xls 
