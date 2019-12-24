8a52dc44eaccc9c9d5ec402fb68b751e81e06ed2
Diego E <despinolaespigares@gmail.com>
2019-12-24 11:47:41 +0100

    .

--- apps/myPortfolio/android/android_termux_exe.sh
+++ apps/myPortfolio/android/android_termux_exe.sh
@@ -7,8 +7,6 @@ echo "******************* installing node modules ..."
 cp storage/shared/temp/appPortfolio.js storage/shared/temp/android
 cd storage/shared/temp/android
 npm install
-cd ..
-cd ..
-cd ..
+cd ../../../..
 echo "******************* Exe node ..."
 node storage/shared/temp/android/appPortfolio.js exe storage/shared/Download/Movements.xls 
