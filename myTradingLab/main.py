633772a8fbea6453f73e2340289c8acee3ee439a
Diego E <despinolaespigares@gmail.com>
2021-08-08 22:41:44 +0200

    .

--- myTradingLab/main.py
+++ myTradingLab/main.py
@@ -23,10 +23,10 @@ class TimeCounter:
 class Acount:
 
   def __init__(self,cast=None):
-    print("Acount created")
     self.resetAcount()
     if (cast is not None):
       self.cast=cast
+    print("Acount created. Cast=",self.cast) 
 
   def resetAcount(self):
     self.cast=0
@@ -44,16 +44,16 @@ class Acount:
     print(' B/L: ',self.benefitLoss) 
     print(' Margin: ',self.margin) 
     print(' Available cast: ',self.available) 
-    print(' Real Cast: ',self.realCast,' (',round((self.realCast*1.00/self.cast)*100,2),'%)') 
+    print(' Real Cast: ',self.realCast,' (used:',round((self.margin*1.00/self.realCast)*100,2),'%)') 
     print(' Open Positions: ',self.openPos)
     #print(self.positions)    
     
   def updateAcount(self):
     self.cast        = self.cast + self.positions.loc[self.positions['Open'] == False,'B/L'].sum()
     self.positions   = self.positions.loc[self.positions['Open'] == True]
-    self.benefitLoss = self.positions.loc[self.positions['Open'] == True,'B/L'   ].sum()
-    self.margin      = self.positions.loc[self.positions['Open'] == True,'Margin'].sum()
-    self.available   = self.cast - self.margin
+    self.benefitLoss = self.positions['B/L'].sum()
+    self.margin      = self.positions['Margin'].sum()
+    self.available   = self.cast - self.margin + self.benefitLoss
     self.realCast    = self.cast + self.benefitLoss
     self.openPos     = len(self.positions)
 
@@ -122,7 +122,7 @@ class MyTradingStrategy:
     acount = Acount(500)   
     pos01 = acount.createPosition(margin=333) 
     pos02 = acount.createPosition(margin=333) 
-    pos03 = acount.createPosition(margin=433)
+    pos03 = acount.createPosition(margin=333)
     #acount.printStatusAcount()    
     acount.updatePosition(id=pos01,bl=5)
     acount.updatePosition(id=pos02,bl=1)
@@ -131,7 +131,7 @@ class MyTradingStrategy:
     acount.updatePosition(id=pos01,bl=-3,stillOpen=False) 
     acount.updatePosition(id=pos02,bl= 1,stillOpen=False)  
     acount.updatePosition(id=pos03,bl=-2,stillOpen=False)  
-    acount.printStatusAcount() 
+    #acount.printStatusAcount() 
     #self.exportResult('mth01')
     print("method01 end")  
 
