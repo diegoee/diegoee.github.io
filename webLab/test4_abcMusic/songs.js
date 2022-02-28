e37ecd2dd7988eaa98a533f326353bfd6bc66584
Diego E <despinolaespigares@gmail.com>
2022-02-28 21:30:55 +0100

    .

--- webLab/test4_abcMusic/songs.js
+++ webLab/test4_abcMusic/songs.js
@@ -2,13 +2,13 @@
 var songs = [];
 var abc=''; 
 
-//values  <z
-
-0-abc=''; 
+abc=''; 
 abc=abc+'X:1\n';
-abc=abc+'T:My Love for Sale Solo (A)\n'; 
+abc=abc+'T:My Love for Sale Solo (Theme A)\n'; 
+abc=abc+'L:1/8\n';
+abc=abc+'%%barnumbers 2\n';  
 abc=abc+'M:C\n';
-abc=abc+'Q:165\n';
+abc=abc+'Q:1/4=165\n';
 abc=abc+'K:Cm\n'; 
 abc=abc+'"F"(c8|c2)z2=A4|"Cm"G4z2E2|C2z2E4|\n';
 abc=abc+'"F"z2F2G4|z2E2C2B,2|"Cm"/(C8|C6)z2|\n';
@@ -19,15 +19,17 @@ songs.push(abc);
 abc=''; 
 abc=abc+'X:1\n';
 abc=abc+'T:My Hot and Humid Solo\n'; 
+abc=abc+'L:1/8\n';
 abc=abc+'M:C\n';
-abc=abc+'Q:126\n';
+abc=abc+'Q:1/4=126\n';
 abc=abc+'K:Em\n'; 
 abc=abc+'B1 B2 A1G1|';
 abc=abc+'"Em"E2z2z1G1E2|z2B1 B1B2A1G1|E2z2B,1^C1E2|z4z2F1G1|\n';
 abc=abc+'"Am"A2z2z1B1A2|z4z2F1G1|A2A2z1G1E2|z3B1 B2 A1G1|\n';
 abc=abc+'"Em"E2z2z1G1E2|z2B1 B1B2A1G1|E2z2B,1^C1E2|z4F1G1|\n'; 
 abc=abc+'"G"_A2 =A1G1  A3 z1 |z2e2 B1A1 G1A1 |"Em" G1E1z2B2E2|z8|]\n';       
-songs.push(abc);   
+songs.push(abc); 
+
 
 //export to window var
 window.songs=songs;
