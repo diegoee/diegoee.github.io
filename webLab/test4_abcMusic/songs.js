dc2f7a78efbaf5ea624687344ad71f7c70527e92
Diego E <despinolaespigares@gmail.com>
2022-03-04 18:57:36 +0100

    .

--- webLab/test4_abcMusic/songs.js
+++ webLab/test4_abcMusic/songs.js
@@ -14,7 +14,7 @@ abc=abc+'"F"(c8|c2)z2=A4|"Cm"G4z2E2|C2z2E4|\n';
 abc=abc+'"F"z2F2G4|z2E2C2B,2|"Cm"/(C8|C6)z2|\n';
 abc=abc+'"F"z1D1F2F1F3|z1D1F2F1F3|"Eb"G4F4|E4C4|\n';
 abc=abc+'"Dm"G8|z4c2G2|"Cm"(C8|C8)|]\n'; 
-songs.push(abc); 
+//songs.push(abc);  
 
 abc=''; 
 abc=abc+'X:1\n';
@@ -23,12 +23,12 @@ abc=abc+'L:1/8\n';
 abc=abc+'M:C\n';
 abc=abc+'Q:1/4=126\n';
 abc=abc+'K:Em\n'; 
-abc=abc+'B1 B2 A1G1|';
-abc=abc+'"Em"E2z2z1G1E2|z2B1 B1B2A1G1|E2z2B,1^C1E2|z4z2F1G1|\n';
-abc=abc+'"Am"A2z2z1B1A2|z4z2F1G1|A2A2z1G1E2|z3B1 B2 A1G1|\n';
-abc=abc+'"Em"E2z2z1G1E2|z2B1 B1B2A1G1|E2z2B,1^C1E2|z4F1G1|\n'; 
-abc=abc+'"G"_A2 =A1G1  A3 z1 |z2e2 B1A1 G1A1 |"Em" G1E1z2B2E2|z8|]\n';       
+abc=abc+'"Em"z1B3A4|B1 A1^c1 z2z1G1|z1G1z1 E1G2E1G1|z4z2F1(A1|\n';
+abc=abc+'"Am"A2)z2z4|z8|z8|z8|\n';
+abc=abc+'"Em"z1B3A4|B1 A1^c1 z2z1E1|z1^F1 z1G1 z1A1 z1G1|E2z2 z2G2|\n'; 
+abc=abc+'"G"z1_A2 =A1G1  A3 z1 |z2e2 B1A1 G1A1 |"Em" G1E1z2B2E2|z8|]\n';       
 songs.push(abc); 
+
 
 
 //export to window var
