9d815895073d92ad24e3bad91284ac7df7e3a871
Diego E <despinolaespigares@gmail.com>
2022-03-05 17:43:47 +0100

    .

--- webLab/test4_abcMusic/songs.js
+++ webLab/test4_abcMusic/songs.js
@@ -2,18 +2,18 @@
 var songs = [];
 var abc=''; 
 
-
 abc=''; 
 abc=abc+'X:1\n';
 abc=abc+'T:My Hot and Humid Solo\n'; 
-abc=abc+'L:1/8\n';
+abc=abc+'L:1/8\n';   
+abc=abc+'%%barnumbers 4\n'; 
 abc=abc+'M:C\n';
 abc=abc+'Q:1/4=126\n';
 abc=abc+'K:Em\n'; 
-abc=abc+'"Em"z1B3A4|B1 A1^c1 z2z1G1|z1G1z1 E1G2E1G1|z4z2F1(A1|\n';
-abc=abc+'"Am"A2)z2z4|z8|z8|z8|\n';
-abc=abc+'"Em"z1B3A4|B1 A1^c1 z2z1E1|z1^F1 z1G1 z1A1 z1G1|E2z2 z2(G2|\n'; 
-abc=abc+'"G"G1)_A2 =A1G1  A3 z1 |z2e2 B1A1 G1A1 |"Em" G1E1z4B2|E2z2z4|]\n';       
+abc=abc+'"Em"z1 B3 A4|"F#m"B2 A1^c1 "B7"z2 z1G1|"Em"z1G1  z1E1  G2  E1G1|"Bm"z4 z2 "E7"F1(A1|\n';
+abc=abc+'"Am"A2) z2 z4|z2 F2 F2 F2|"F#m"F2 A2 z2 G1E1|"B7" z8|\n';
+abc=abc+'"Em"z1 B3 A4|"F#m"B2 A1^c1 "B7"z2 z1E1|"Em"z1^F1 z1G1 z1A1 z1G1|"Bm"E2z2 "E7"z2(G2|\n'; 
+abc=abc+'"G"G2) _B2 =A1G1 A2|"F#m"z2 e2 "B7"B1A1 G1A1|"Em" G1E1 z4 B2|"F#m"E2z2 "Bm"z4 |]\n';       
 songs.push(abc); 
 
 abc=''; 
@@ -29,7 +29,6 @@ abc=abc+'"F"z2F2G4|z2E2C2B,2|"Cm"/(C8|C6)z2|\n';
 abc=abc+'"F"z1D1F2F1F3|z1D1F2F1F3|"Eb"G4F4|E4C4|\n';
 abc=abc+'"Dm"G8|z4c2G2|"Cm"(C8|C8)|]\n'; 
 songs.push(abc);  
-
 
 //export to window var
 window.songs=songs;
