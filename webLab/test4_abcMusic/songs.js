f2e082a49ee3f041300708eeb4dc55c16d4eacdc
Diego E <despinolaespigares@gmail.com>
2022-04-29 17:06:29 +0200

    .

--- webLab/test4_abcMusic/songs.js
+++ webLab/test4_abcMusic/songs.js
@@ -10,15 +10,15 @@ abc=abc+'%%barnumbers 4\n';
 abc=abc+'M:4/4\n';
 abc=abc+'Q:1/4=75\n';
 abc=abc+'K:Am\n'; 
-abc=abc+'     z4 E1G2(A1 A1)c3 B2(A2    |"Am"A2) A6 "Em7"z4"G"z4        |"Am" z4 E1G2(A1 A3)(c1 c1)B2G1 |"Am"A8 "Em7"z4"G"z4            |\n';
-abc=abc+'"Am" z4 A1A2(c1 c1)e3 d2e2     |"Em"d4 z1 A1d1(c1 c2)A2G2A2    |"Dm7"z4 c1d1(c2 c4) A1G2A1     |"Am"A8 "Em7"z4"G"z4            |\n'; 
-abc=abc+'"Am" z4 E1G3 A1c3 B4           |"Am"A2  A6 "Em7"z4"G"z4        |"Am" z4 E1G3 A1c3 B4           |"Am"G2  A6 "Em7"z4"G"z4        |\n';
-abc=abc+'"Am" z4 A1A2(c1 c1)e3 d2e2     |"Em"d4 z1 A1d1(c1 c2)A2G2A2    |"Dm7"z4 c1d1(c2 c4) A1G2A1     |"Am"A8 "Em7"z4"G"z4            |\n';  
-abc=abc+'"Am" z3 E1 G1A2G1 A2G1A2G1A2   |G1A2G1 A2G1A2G1A1E2 G1A2G1     |G1A2G1 A2G1A2G1A1E2 G1A2G1     |G1A2G1 A2G1A2G1A1E2 G1A2G1     |\n';
-abc=abc+'"Am" G1A2G1 A2G1A2G1A1E2 G1A2G1|"Em"z4 z1 A1d1(c1 c2)A2G2A2    |"Dm7"z4 c1d1(c2 c4) A1G2A1     |"Am"A8 "Em7"z4"G"z4            |]\n'; 
-abc=abc+'"Am" z4 E1G2(A1 A1)c3 B2(A2    |"Am"A2) A6 "Em7"z4"G"z4        | z4 E1G2(A1 A3)(c1 c1)B2G1     |"Am"A8 "Em7"z4"G"z4            |\n';
-abc=abc+'"Am" z4 A1A2(c1 c1)e3 d2e2     |"Em"d4 z1 A1d1(c1 c2)A2G2A2    |"Dm7"z4 c1d1(c2 c4) A1G2A1     |"Am"A8 "Em7"z4"G"z4            |\n'; 
-abc=abc+'"Am"z4 c1d1(c2 c4) A1G2A1      |"Am"A8 "Em7"z4"G"z4            |"Am"z4 c1d1(c2 c4) A1G2A1      |"Am"A8 z8                      |\n';  
+abc=abc+'     z4 E1G2A1- A1c3 B2A2-     |"Am"A2 A6 "Em7"z4"G"z4     |"Am" z4 E1G2A1- A3c1- c1B2G1 |"Am"A8 "Em7"z4"G"z4        |\n';
+abc=abc+'"Am" z4 A1A2c1- c1e3 d2e2      |"Em"d4 z1 A1d1c1- c2A2G2A2 |"Dm7"z4 c1d1c2- c4 A1G2A1    |"Am"A8 "Em7"z4"G"z4        |\n'; 
+abc=abc+'"Am" z4 E1G3 A1c3 B4           |"Am"A2  A6 "Em7"z4"G"z4    |"Am" z4 E1G3 A1c3 B4         |"Am"G2  A6 "Em7"z4"G"z4    |\n';
+abc=abc+'"Am" z4 A1A2c1- c1e3 d2e2      |"Em"d4 z1 A1d1c1- c2A2G2A2 |"Dm7"z4 c1d1c2- c4 A1G2A1    |"Am"A8 "Em7"z4"G"z4        |\n';  
+abc=abc+'"Am" z3 E1 G1A2G1 A2G1A2G1A2   |G1A2G1 A2G1A2G1A1E2 G1A2G1 |G1A2G1 A2G1A2G1A1E2 G1A2G1   |G1A2G1 A2G1A2G1A1E2 G1A2G1 |\n';
+abc=abc+'"Am" G1A2G1 A2G1A2G1A1E2 G1A2G1|"Em"z4 z1 A1d1c1- c2A2G2A2 |"Dm7"z4 c1d1c2- c4) A1G2A1   |"Am"A8 "Em7"z4"G"z4        |]\n'; 
+abc=abc+'"Am" z4 E1G2A1- A1c3 B2A2-     |"Am"A2- A6 "Em7"z4"G"z4    | z4 E1G2A1- A3c1- c1B2G1     |"Am"A8 "Em7"z4"G"z4        |\n';
+abc=abc+'"Am" z4 A1A2c1- c1e3 d2e2      |"Em"d4 z1 A1d1c1- c2A2G2A2 |"Dm7"z4 c1d1c2- c4 A1G2A1    |"Am"A8 "Em7"z4"G"z4        |\n'; 
+abc=abc+'"Am"z4 c1d1c2- c4 A1G2A1       |"Am"A8 "Em7"z4"G"z4        |"Am"z4 c1d1c2- c4 A1G2A1     |"Am"A8 z8                  |\n';  
 songs.push(abc); 
 
 abc=''; 
@@ -44,13 +44,11 @@ abc=abc+'%%barnumbers 4\n';
 abc=abc+'M:C\n';
 abc=abc+'Q:1/4=126\n';
 abc=abc+'K:Em\n'; 
-abc=abc+'"Em"z1 B3 A4|"F#m"B2 A1^c1 "B7"z2 z1G1|"Em"z1G1  z1E1  G2  E1G1|"Bm"z4 z2 "E7"F1(A1|\n';
-abc=abc+'"Am"A2) z2 z4|z2 F2 F2 F2|"F#m"F2 A2 z2 G1E1|"B7" z8|\n';
-abc=abc+'"Em"z1 B3 A4|"F#m"B2 A1^c1 "B7"z2 z1E1|"Em"z1^F1 z1G1 z1A1 z1G1|"Bm"E2z2 "E7"z2(G2|\n'; 
-abc=abc+'"G"G2) _B2 =A1G1 A2|"F#m"z2 e2 "B7"B1A1 G1A1|"Em" G1E1 z4 B2|"F#m"E2z2 "Bm"z4 |]\n';       
-songs.push(abc); 
-
-
+abc=abc+'"Em"z1 B3 A4      |"F#m"B2 A1^c1 "B7"z2 z1G1|"Em"z1G1  z1E1  G2  E1G1|"Bm"z4 z2 "E7"F1A1-|\n';
+abc=abc+'"Am"A2 z2 z4      |z2 F2 F2 F2              |"F#m"F2 A2 z2 G1E1      |"B7" z8            |\n';
+abc=abc+'"Em"z1 B3 A4      |"F#m"B2 A1^c1 "B7"z2 z1E1|"Em"z1^F1 z1G1 z1A1 z1G1|"Bm"E2z2 "E7"z2G2- |\n'; 
+abc=abc+'"G"G2 _B2 =A1G1 A2|"F#m"z2 e2 "B7"B1A1 G1A1 |"Em" G1E1 z4 B2         |"F#m"E2z2 "Bm"z4   |]\n';       
+songs.push(abc);  
 
 abc=''; 
 abc=abc+'X:1\n';
@@ -60,10 +58,10 @@ abc=abc+'%%barnumbers 2\n';
 abc=abc+'M:C\n';
 abc=abc+'Q:1/4=165\n';
 abc=abc+'K:Cm\n'; 
-abc=abc+'"F"(c8|c2)z2=A4|"Cm"G4z2E2|C2z2E4|\n';
-abc=abc+'"F"z2F2G4|z2E2C2B,2|"Cm"/(C8|C6)z2|\n';
-abc=abc+'"F"z1D1F2F1F3|z1D1F2F1F3|"Eb"G4F4|E4C4|\n';
-abc=abc+'"Dm"G8|z4c2G2|"Cm"(C8|C8)|]\n'; 
+abc=abc+'"F"c8-        |c2z2=A4   |"Cm"G4z2E2 |C2z2E4 |\n';
+abc=abc+'"F"z2F2G4     |z2E2C2B,2 |"Cm"/C8-   |C6z2   |\n';
+abc=abc+'"F"z1D1F2F1F3 |z1D1F2F1F3|"Eb"G4F4   |E4C4   |\n';
+abc=abc+'"Dm"G8        |z4c2G2    |"Cm"C8-    |C4  z4 |]\n'; 
 songs.push(abc);  
 
 //export to window var
