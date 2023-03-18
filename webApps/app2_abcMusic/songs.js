315a9fe01880a25a0c012745f9a758761372d265
Diego E <despinolaespigares@gmail.com>
2023-03-18 15:03:31 +0100

    .

--- webApps/app2_abcMusic/songs.js
+++ webApps/app2_abcMusic/songs.js
@@ -2,6 +2,59 @@
 var songs = [];
 var abc='';  
 
+/*
+abc=''; 
+abc=abc+'X:1\n';
+abc=abc+'T:As Time Goes By\n';
+abc=abc+'M:4/4\n';
+abc=abc+'L:1/8\n';
+abc=abc+'K:F\n';
+abc=abc+'G| AG F E2 F3| GB AG F A3| Be dc B c3-| c3 z2 z| d| fe dc d2 e2| B2 B2 E2 F2|]\n';
+abc=abc+'G8-| G6 z G| AG FE F3 G| BA GF A3 B| ed cB c4| z4 z2 z d| fe dc d2 e2|]\n';
+abc=abc+'B2 B2 E2 F2| E8-| E4 z4| E F3/2 E/2 c3 c2| c_d c=B c4| FG F c3 c2|]\n';
+abc=abc+'c_d c=B c4| GA G e3 e2| ed ed f2 d2| c2 c2 G2 G2| B6 z G| AG FE F F2 G|]\n';
+abc=abc+'BA GF A A2 B| ed cB c4| z6 d2| fe dc d2 e2| B2 B4 G2| B4 B4| e6 z2|]\n';  
+songs.push(abc);
+*/
+
+abc=''; 
+abc=abc+'X:1\n';
+abc=abc+'T: All of Me (no Chords)\n'; 
+abc=abc+'C: --\n'; 
+abc=abc+'L:1/16\n';     
+abc=abc+'M:4/4\n';
+abc=abc+'Q:1/4=135\n';
+abc=abc+'K:D\n';    
+abc=abc+'   d4 A2F2- F8- |F8      (3d4e4d4  |c4 ^A2F2- F8- |F16   |\n';   
+abc=abc+'   B6 A2 F8-    |F4  ^E4 (3F4=c4B4 |A8 G8-        |G16   |\n';  
+abc=abc+'   F6 =F2 E8-   |E8      (3F4^A4c4 |e8 d8-        |d16   |\n';  
+abc=abc+'   c6 =c2 B8-   |B8      (3B4e4c4  |B16           |c16   |\n';  
+abc=abc+'   d4 A2F2- F8- |F8      (3d4e4d4  |c4 ^A2F2- F8- |F16   |\n';  
+abc=abc+'   B4 A2F2- F8- |F4  ^E4 (3F4=c4B4 |A8 G8-        |G16   |\n'; 
+abc=abc+'   e8 d4 c4     |e12 c4            |B8  F4 A4     |c12 B4|\n';  
+abc=abc+'   d8 B4 d4     |f8 f8             |d16-          |d16   |]\n';   
+songs.push(abc);
+
+
+abc=''; 
+abc=abc+'X:1\n';
+abc=abc+'T: All of Me\n'; 
+abc=abc+'C: --\n'; 
+abc=abc+'L:1/16\n';     
+abc=abc+'M:4/4\n';
+abc=abc+'Q:1/4=135\n';
+abc=abc+'K:D\n';    
+abc=abc+'"D"d4 A2F2- F8- |F8      (3d4e4d4  |"F#7"c4 ^A2F2- F8-|    F16|\n';   
+abc=abc+'"B7"B6 A2 F8-   |F4  ^E4 (3F4=c4B4 |"E-" A8 G8-       |    G16|\n';  
+abc=abc+'"F#7"F6 =F2 E8- |E8      (3F4^A4c4 |"B-" e8 d8-       |    d16|\n';  
+abc=abc+'"E7" c6 =c2 B8- |B8      (3B4e4c4  |"E-7"B16          |"A7"c16|\n';  
+abc=abc+'"D"d4 A2F2- F8-|F8      (3d4e4d4  |"F#7"c4 ^A2F2- F8- |F16    |\n';  
+abc=abc+'"B7"B4 A2F2- F8-|F4  ^E4 (3F4=c4B4 |"E-" A8 G8-       |G16    |\n'; 
+abc=abc+'"G" e8 d4 c4    |"G-"e12 c4        |"D7"B8 "F#-7"F4 A4|"B7"c12 B4|\n';  
+abc=abc+'"E-7"d8 B4 d4   |"A7"f8 f8         |"D6"d16-          |d16   |]\n';   
+songs.push(abc);
+
+
 abc=''; 
 abc=abc+'X:1\n';
 abc=abc+'T: Stand by Me\n'; 
