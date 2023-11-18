ed1ef1d1efcfb46cd11a24f9548593062a2b2b94
Diego E <despinolaespigares@gmail.com>
2023-11-18 23:03:16 +0100

    .

--- javascriptTesting/javascriptTest01.js
+++ javascriptTesting/javascriptTest01.js
@@ -1,49 +1,49 @@
 function createJsonTree(folderFiles){    
-    var jsonTree = [];  
-    for (var i in folderFiles){
-      var folder = folderFiles[i].split("/"); 
-      for (var ii=0; ii<folder.length; ii++){
-        var add = true; 
-        for (var iii=0; iii<jsonTree.length; iii++){
-          if(jsonTree[iii].id===(ii+"-"+folder[ii])){
-            add = false;
-            break;
-          }
-        }
-        if(add){
-          if(ii==0){
-            jsonTree.push({ 
-              id: (ii+"-"+folder[ii]), 
-              parent: '#', 
-              text: folder[ii]
-            });  
-          }else{
-            jsonTree.push({ 
-              id: (ii+"-"+folder[ii]), 
-              parent: ((ii-1)+"-"+folder[ii-1]), 
-              text: folder[ii]
-            });  
-          }
+  var jsonTree = [];  
+  for (var i in folderFiles){
+    var folder = folderFiles[i].split("/"); 
+    for (var ii=0; ii<folder.length; ii++){
+      var add = true; 
+      for (var iii=0; iii<jsonTree.length; iii++){
+        if(jsonTree[iii].id===(ii+"-"+folder[ii])){
+          add = false;
+          break;
         }
       }
-    } 
-    return jsonTree;
-  }
+      if(add){
+        if(ii==0){
+          jsonTree.push({ 
+            id: (ii+"-"+folder[ii]), 
+            parent: '#', 
+            text: folder[ii]
+          });  
+        }else{
+          jsonTree.push({ 
+            id: (ii+"-"+folder[ii]), 
+            parent: ((ii-1)+"-"+folder[ii-1]), 
+            text: folder[ii]
+          });  
+        }
+      }
+    }
+  } 
+  return jsonTree;
+}
   
-  function exe(){
-    console.log("**** START javascriptTest *****"); 
-    var folderFiles = [];
-    folderFiles.push('Principal/folder1/Report1');
-    folderFiles.push("Principal/folder2/Report2");
-    folderFiles.push("Principal/folder2/folder21/Report3");
-    folderFiles.push("Principal/folder2/folder22/Report4");
-    folderFiles.push("Principal/folder2/folder3/Report4");
-    folderFiles.push("Principal/folder3/Report5");
-    folderFiles.push("Principal/folder3/Report6");
-    folderFiles.push("Principal/folder3/Report7"); 
-    console.log(folderFiles);
-    var folderFilesJSON = createJsonTree(folderFiles);
-    console.log(folderFilesJSON);
-    console.log("**** START javascriptTest *****"); 
-  }
-  exe();
+function exe(){
+  console.log("**** START javascriptTest01 *****"); 
+  var folderFiles = [];
+  folderFiles.push('Principal/folder1/Report1');
+  folderFiles.push("Principal/folder2/Report2");
+  folderFiles.push("Principal/folder2/folder21/Report3");
+  folderFiles.push("Principal/folder2/folder22/Report4");
+  folderFiles.push("Principal/folder2/folder3/Report4");
+  folderFiles.push("Principal/folder3/Report5");
+  folderFiles.push("Principal/folder3/Report6");
+  folderFiles.push("Principal/folder3/Report7"); 
+  console.log(folderFiles);
+  var folderFilesJSON = createJsonTree(folderFiles);
+  console.log(folderFilesJSON);
+  console.log("**** START javascriptTest01 *****"); 
+}
+exe();
---
+++ javascriptTesting/javascriptTest02.js
@@ -0,0 +1,8 @@
+function mth1(){     
+}
+
+function exe(){
+  console.log("**** START javascriptTest02 *****");  
+  console.log("**** START javascriptTest02 *****"); 
+}
+exe();
