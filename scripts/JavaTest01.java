f5a1a9029879b805ee9112700bdfcdfaf19cef08
Diego E <despinolaespigares@gmail.com>
2023-11-27 22:22:17 +0100

    .

--- scripts/JavaTest01.java
+++ scripts/JavaTest01.java
@@ -1,58 +1,22 @@
-package javaTesting;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors; 
+package scripts;
 
 public class JavaTest01{
-  public List<String> folderFiles;
-  public String       jsonTree; 
+  public String var; 
 
-  public JavaTest01() { 
-    folderFiles  = new ArrayList<String>();
-    //https://www.jstree.com/
-    folderFiles.add("Principal/folder1/Report1");
-    folderFiles.add("Principal/folder2/Report2");
-    folderFiles.add("Principal/folder2/folder21/Report3");
-    folderFiles.add("Principal/folder2/folder22/Report4");
-    folderFiles.add("Principal/folder2/folder3/Report4");
-    folderFiles.add("Principal/folder3/Report5");
-    folderFiles.add("Principal/folder3/Report6");
-    folderFiles.add("Principal/folder3/Report7"); 
+  public JavaTest01() {  
+    var = "Hellooo";
+    System.out.println("var: "+var);  
+    changeVar("Olé");
+    System.out.println("var: "+var);  
   }
 
-  public void createJsonTree(){
-    jsonTree="[";  
-    List<String> aux = new ArrayList<String>();
-
-    for (String folderFile: folderFiles){
-      String[] folder = folderFile.split("/"); 
-      for (int ii = 0; ii < folder.length; ii++){
-        if(ii==0){
-          aux.add("{  id: \""+(ii+"-"+folder[ii])+"\", parent: \"#\", text: \""+(folder[ii])+"\" }");  
-        }else{
-          aux.add("{  id: \""+(ii+"-"+folder[ii])+"\", parent: \""+((ii-1)+"-"+folder[ii-1])+"\", text: \""+(folder[ii])+"\" }"); 
-        }
-      }
-    } 
-    // distinct
-    aux = aux.stream().distinct().collect(Collectors.toList()); 
-
-    for (String a: aux){
-      jsonTree=jsonTree+a+", ";
-    }
-    jsonTree=jsonTree+"]";
-    
+  public void changeVar(String newVar){
+    var = newVar;
   }
 
   public static void main(String[] args){
-    System.out.println("**** START JavaTest *****"); 
-
-    JavaTest01 test = new JavaTest01();    
-    System.out.println("folderFiles len: "+test.folderFiles.size());  
-    test.createJsonTree();
-    System.out.println("jsonTree: "+test.jsonTree); 
-
-    System.out.println("**** END   JavaTest *****"); 
+    System.out.println("**** START JavaTest01 *****");  
+    new JavaTest01();   
+    System.out.println("**** END   JavaTest01 *****"); 
   }
 } 
