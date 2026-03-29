2045bc82db4a4cae8aced40444f79e9081416a5c
Diego E. <despinolaespigares@gmail.com>
2026-03-30 00:02:55 +0200

    .

--- app_mygiro/app_myGiroValues.bat
+++ app_mygiro/app_myGiroValues.bat
@@ -7,14 +7,14 @@ title script_name
 echo Script root: %script_root%
 echo Script name: %script_name%.js
 echo %cd%
-::rmdir /S /Q node_modules 
-::del package-lock.json 
-::call npm install 
+rmdir /S /Q node_modules 
+del package-lock.json 
+call npm install 
 call node %cd%/%script_name%.js  
 timeout 1   
 set webNavegator="C:\Program Files\Google\Chrome\Application\chrome.exe"
 call %webNavegator% %cd%\%script_name%_result.html 
 timeout 1  
 del %cd%\%script_name%_result.html
-::rmdir /S /Q node_modules 
-::del package-lock.json 
+rmdir /S /Q node_modules 
+del package-lock.json 
